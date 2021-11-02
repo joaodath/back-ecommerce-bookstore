af183a1419d0577df0c2b9b00d8daa49b17a9717
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-01 21:53:32 -0300

    feat: usar bcrypt para salvar a senha do usuario como hash

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,13 +3,19 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Prisma, User } from '.prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
   constructor(private db: PrismaService) {}
 
   async create(createUserDto: Prisma.UserCreateInput): Promise<User> {
-    return await this.db.user.create({ data: createUserDto });
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    return await this.db.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+      },
+    });
   }
 
   async findAll(): Promise<User[]> {
