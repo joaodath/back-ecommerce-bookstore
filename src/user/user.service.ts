0249337b2445a40fa7d0437b12609bf1b5efa1ed
João Rodrigues <4998756+joaodath@users.noreply.github.com>
2021-11-03 21:24:10 -0300

    Revert "Revert "feat: crud produtos / npm lint""

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { Prisma, User } from '.prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
