ca6c007151d6bcf1873da1136822ef4caabc6c5c
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-04 21:27:02 -0300

    feat: tratar erros de conflito @unique no schema

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Prisma, User } from '.prisma/client';
@@ -7,8 +11,29 @@ import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
   constructor(private db: PrismaService) {}
-
   async create(createUserDto: Prisma.UserCreateInput): Promise<User> {
+    const usernameExists = await this.db.user.findUnique({
+      where: { username: createUserDto.username },
+    });
+    if (usernameExists) {
+      throw new ConflictException(
+        `Username ${createUserDto.username} already exists`,
+      );
+    }
+    const emailExists = await this.db.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+    if (emailExists) {
+      throw new ConflictException(
+        `Email ${createUserDto.email} already exists`,
+      );
+    }
+    const cpfExists = await this.db.user.findUnique({
+      where: { cpf: createUserDto.cpf },
+    });
+    if (cpfExists) {
+      throw new ConflictException(`CPF ${createUserDto.cpf} already exists`);
+    }
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
     return await this.db.user.create({
       data: {
