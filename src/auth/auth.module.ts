a0608ff07c75868191938ba1185624304cdd4f94
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-06 03:35:29 -0300

    feat: mover tempo de validade do jwt para variavel de ambiente

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { AuthController } from './auth.controller';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '604800s' },
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
