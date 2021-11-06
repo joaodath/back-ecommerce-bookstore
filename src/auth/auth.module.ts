88d2f4cee70ea01c3e86535ae4f56c3ef8f4be7c
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-06 03:22:17 -0300

    feat: mover logica da rota login para o auth.controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
@@ -18,5 +19,6 @@ import { jwtConstants } from './constants';
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
