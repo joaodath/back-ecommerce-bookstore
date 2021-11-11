c52ed06d5990b9b17d06b4074787a43a209c18bc
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-11 20:07:07 -0300

    feat: iniciar recurso checkout com nest g

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
 import { ShoppingHistoryModule } from './shopping-history/shopping-history.module';
+import { CheckoutModule } from './checkout/checkout.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { ShoppingHistoryModule } from './shopping-history/shopping-history.modul
     AuthModule,
     BooksModule,
     ShoppingHistoryModule,
+    CheckoutModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
