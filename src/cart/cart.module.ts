1d6961824f5d1fd342e1f6e689fdffa97b356ff6
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-12 22:46:53 -0300

    fix: alterar CartService para ShoppingCartService que é o nome certo do service

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CartService } from './cart.service';
+import { ShoppingCartService } from './cart.service';
 import { CartController } from './cart.controller';
 
 @Module({
   controllers: [CartController],
-  providers: [CartService],
+  providers: [ShoppingCartService],
+  exports: [ShoppingCartService],
 })
 export class CartModule {}
