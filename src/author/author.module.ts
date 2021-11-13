f5b19f943a6abaaff3ec7cdaf55673e38f1b159d
Jean-Martins22 <jeanmartins.unai.melo@hotmail.com>
2021-11-13 20:28:53 -0300

    feat: criar module para author

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthorService } from './author.service';
 import { AuthorController } from './author.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AuthorController],
-  providers: [AuthorService]
+  providers: [AuthorService, PrismaService],
 })
 export class AuthorModule {}
