a0930cbde67e3db261d8416dc2fc04c4ee2ae9b9
Jean-Martins22 <jeanmartins.unai.melo@hotmail.com>
2021-11-13 21:58:27 -0300

    feat: atualizar module do publisher

--- src/publisher/publisher.module.ts
+++ src/publisher/publisher.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PublisherService } from './publisher.service';
 import { PublisherController } from './publisher.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PublisherController],
-  providers: [PublisherService]
+  providers: [PublisherService, PrismaService],
 })
 export class PublisherModule {}
