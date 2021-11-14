fc5e47c0c2f55abaca27f3166cbc45c74a8f7fb9
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-13 22:18:23 -0300

    fix: organizar importações faltando faltava importar PrismaModule, PrismaService,  BooksModule e BooksService no contexto de  cart-module

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ShoppingCartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ShoppingCartItemsService } from 'src/cart-items/cart-items.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { CartItemsModule } from 'src/cart-items/cart-items.module';
+import { BooksService } from 'src/books/books.service';
+import { BooksModule } from 'src/books/books.module';
 
 @Module({
+  imports: [PrismaModule, CartItemsModule, BooksModule],
   controllers: [CartController],
-  providers: [ShoppingCartService],
+  providers: [
+    ShoppingCartService,
+    PrismaService,
+    ShoppingCartItemsService,
+    BooksService,
+  ],
   exports: [ShoppingCartService],
 })
 export class CartModule {}
