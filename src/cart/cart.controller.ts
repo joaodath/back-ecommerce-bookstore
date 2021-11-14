d299616fb32645bc14c5ec4098353290e46a8a90
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-14 11:12:14 -0300

    feat: implementa autenticação nas rotas existentes

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -8,30 +8,53 @@ import {
   ParseIntPipe,
   Request,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ShoppingCartService } from './cart.service';
 import { Prisma, ShoppingCart, ShoppingCartItems } from '.prisma/client';
 import { AddItemDto } from './dto/add-item.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreateUserCartDto } from './dto/create-user-cart.dto';
 
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: ShoppingCartService) {}
 
-  @Post()
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // async createCart(
+  //   @Body() createCartDto: Prisma.ShoppingCartCreateInput,
+  // ): Promise<ShoppingCart> {
+  //   return await this.cartService.createCart(createCartDto);
+  // }
+
+  @Post('new/anon')
   @UsePipes(ValidationPipe)
-  async createCart(
-    @Body() createCartDto: Prisma.ShoppingCartCreateInput,
-  ): Promise<ShoppingCart> {
-    return await this.cartService.createCart(createCartDto);
+  async createAnonCart(): Promise<ShoppingCart> {
+    return await this.cartService.createAnonCart();
   }
 
-  @Post('/add')
+  @Post('new/user')
+  @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
-  async addItem(
+  async createUserCart(
+    @Request() req,
+    @Body() createUserCartDto: CreateUserCartDto,
+  ): Promise<ShoppingCart> {
+    return await this.cartService.createUserCart(
+      req.user.username,
+      createUserCartDto,
+    );
+  }
+
+  @Post('user/item/add')
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(ValidationPipe)
+  async addItemUser(
     @Request() req,
     @Body() addItemDto: AddItemDto,
   ): Promise<ShoppingCart> {
-    return await this.cartService.addItem(req.user.username, addItemDto);
+    return await this.cartService.addItemUser(req.user.username, addItemDto);
   }
 
   @Delete('item/:id')
