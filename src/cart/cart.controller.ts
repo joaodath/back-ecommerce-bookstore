e99fca3c0ac022c5fe5527c48cbd1e5e71d81fd5
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-14 11:30:31 -0300

    feat: crar rotas updateItemUser, addItemAnon e updateItemAnon

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -9,12 +9,14 @@ import {
   Request,
   Delete,
   UseGuards,
+  Patch,
 } from '@nestjs/common';
 import { ShoppingCartService } from './cart.service';
 import { Prisma, ShoppingCart, ShoppingCartItems } from '.prisma/client';
 import { AddItemDto } from './dto/add-item.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateUserCartDto } from './dto/create-user-cart.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
 
 @Controller('cart')
 export class CartController {
@@ -55,6 +57,33 @@ export class CartController {
     @Body() addItemDto: AddItemDto,
   ): Promise<ShoppingCart> {
     return await this.cartService.addItemUser(req.user.username, addItemDto);
+  }
+
+  @Patch('user/item/update')
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(ValidationPipe)
+  async updateItemUser(
+    @Request() req,
+    @Body() updateItemDto: UpdateItemDto,
+  ): Promise<ShoppingCart> {
+    return await this.cartService.updateItemUser(
+      req.user.username,
+      updateItemDto,
+    );
+  }
+
+  @Post('anon/item/add')
+  @UsePipes(ValidationPipe)
+  async addItemAnon(@Body() addItemDto: AddItemDto): Promise<ShoppingCart> {
+    return await this.cartService.addItemAnon(addItemDto);
+  }
+
+  @Patch('anon/item/update')
+  @UsePipes(ValidationPipe)
+  async updateItemAnon(
+    @Body() updateItemDto: UpdateItemDto,
+  ): Promise<ShoppingCart> {
+    return await this.cartService.updateItemAnon(updateItemDto);
   }
 
   @Delete('item/:id')
