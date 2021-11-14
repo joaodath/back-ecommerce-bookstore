8cf82446bb9cf099e5fd8ca733be8bfc796b464d
Jean-Martins22 <jeanmartins.unai.melo@hotmail.com>
2021-11-13 21:12:36 -0300

    feat: importar author no books

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,10 +3,12 @@ import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthorService } from 'src/author/author.service';
+import { AuthorModule } from 'src/author/author.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthorModule],
   controllers: [BooksController],
-  providers: [BooksService, PrismaService],
+  providers: [BooksService, PrismaService, AuthorService],
 })
 export class BooksModule {}
