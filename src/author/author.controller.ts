880a16f5b126ad786e614614a300445eb4110316
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2021-11-16 13:33:58 -0300

    Update src/author/author.controller.ts
    Co-authored-by: João Rodrigues <4998756+joaodath@users.noreply.github.com>

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -14,6 +14,7 @@ import { AuthorService } from './author.service';
 import { Authors } from '.prisma/client';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('author')
 export class AuthorController {
