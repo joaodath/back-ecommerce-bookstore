af0b48a15cedb3ecd4033998798a68c961d05cff
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2021-11-16 13:40:55 -0300

    Update src/category/category.service.ts
    Co-authored-by: João Rodrigues <4998756+joaodath@users.noreply.github.com>

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -87,7 +87,6 @@ export class CategoryService {
   }
 
   async removeBook(removeBook: RemoveBookCategoryDto): Promise<Books> {
-      where: { id: removeBook.bookId },
       data: {
         category: {
           disconnect: {
