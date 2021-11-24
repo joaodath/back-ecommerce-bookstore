2f556be290f5384f20e4b08cf6565b5bd56c0fc1
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-24 18:54:22 -0300

    fix: cartId opcional no CreateUserCartDto

--- src/cart/dto/create-user-cart.dto.ts
+++ src/cart/dto/create-user-cart.dto.ts
@@ -1,6 +1,7 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class CreateUserCartDto {
+  @IsOptional()
   @IsNumber()
-  cartId?: number;
+  cartId: number;
 }
