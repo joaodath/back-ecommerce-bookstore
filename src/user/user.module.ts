e33e53d13f17f088043132b1d8b057c028b413f4
cnicacio <carlosnicacio94@gmail.com>
2021-11-24 12:56:59 -0300

    feat: incluir AddressesService nos providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AddressesService } from 'src/addresses/addresses.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -7,7 +8,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, AddressesService],
   exports: [UserService],
 })
 export class UserModule {}
