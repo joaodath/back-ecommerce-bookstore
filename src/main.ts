1b5c03ddc1a00ae8eb9f55172fdb94fb6c1061af
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-24 20:14:47 -0300

    feat(swagger): setup inicial

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -12,6 +13,21 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend para eCommerce de livros em NestJS')
+    .setDescription(
+      'Projeto de backend para ecommerce desenvolvido no Bootcamp 2021 C005 da Blue Edtech para a OSF.',
+    )
+    .setVersion('0.5')
+    .addTag('ecommerce')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
 }
