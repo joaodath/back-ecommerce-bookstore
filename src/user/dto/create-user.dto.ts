e953b4704e7ffc4ca5e6594e56f01deed775a58e
cnicacio <carlosnicacio94@gmail.com>
2021-11-23 23:34:53 -0300

    feat: remover dados de endereço do dto do usuário

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -34,20 +34,5 @@ export class CreateUserDto {
   cpf: string;
 
   @IsString()
-  cep: string;
-
-  @IsString()
-  country: string;
-
-  @IsString()
-  state: string;
-
-  @IsString()
-  city: string;
-
-  @IsString()
-  address: string;
-
-  @IsString()
   phonenumber: string;
 }
