b766d209356daf65c9b1e22090dcf328e01527fc
Jean-Martins22 <jeanmartins.unai.melo@hotmail.com>
2021-11-25 19:21:56 -0300

    feat: swagger na rota auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Request, UseGuards, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
