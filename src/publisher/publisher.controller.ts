c68852773cb2b54ce486410405695490824a02de
Jean-Martins22 <jeanmartins.unai.melo@hotmail.com>
2021-11-25 23:13:21 -0300

    feat: adicionar ApiResponse no publisher controller

--- src/publisher/publisher.controller.ts
+++ src/publisher/publisher.controller.ts
@@ -16,13 +16,14 @@ import { Publisher } from '.prisma/client';
 import { CreatePublisherDto } from './dto/create-publisher.dto';
 import { UpdatePublisherDto } from './dto/update-publisher.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Publisher')
 @Controller('publisher')
 export class PublisherController {
   constructor(private readonly publisherService: PublisherService) {}
 
+  @ApiResponse({ status: 201, description: 'Recurso criado' })
   @Post('new')
   //@UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
@@ -32,24 +33,28 @@ export class PublisherController {
     return await this.publisherService.create(createPublisherDto);
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Get('all')
   @UsePipes(ValidationPipe)
   async findAll(): Promise<Publisher[]> {
     return this.publisherService.findAll();
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Get('id/:id')
   @UsePipes(ValidationPipe)
   async findUnique(@Param('id', ParseIntPipe) id: number): Promise<Publisher> {
     return this.publisherService.findUnique(id);
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Get('name/:name')
   @UsePipes(ValidationPipe)
   async findName(@Param('name') name: string): Promise<Publisher[]> {
     return await this.publisherService.findByName(name);
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Patch('update/:id')
   //@UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
@@ -60,6 +65,7 @@ export class PublisherController {
     return this.publisherService.update(id, updatePublisherDto);
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Delete('delete/:id')
   //@UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
