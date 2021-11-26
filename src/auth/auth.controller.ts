5fd9c1630a63f5f58812653ac6603dd025c9349e
Jean-Martins22 <jeanmartins.unai.melo@hotmail.com>
2021-11-25 23:09:24 -0300

    feat: adicionar ApiResponse no auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,19 +2,25 @@ import { Controller, Request, UseGuards, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiHeader, ApiResponse } from '@nestjs/swagger';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiResponse({ status: 201, description: 'Recurso criado' })
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'JWT Token',
+  })
   @UseGuards(JwtAuthGuard)
   @Get('test')
   getProfile(@Request() req) {
