ad54fff1513de4f2e3d2127602fa1d8d701afb05
Jean-Martins22 <jeanmartins.unai.melo@hotmail.com>
2021-11-25 23:10:52 -0300

    feat: adicionar ApiResponse no author controller

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -16,13 +16,14 @@ import { Authors } from '.prisma/client';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 
 @ApiTags('Author')
 @Controller('author')
 export class AuthorController {
   constructor(private readonly authorService: AuthorService) {}
 
+  @ApiResponse({ status: 201, description: 'Recurso criado' })
   @Post('new')
   //@UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
@@ -30,24 +31,28 @@ export class AuthorController {
     return await this.authorService.create(createAuthorDto);
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Get('all')
   @UsePipes(ValidationPipe)
   async findAll(): Promise<Authors[]> {
     return await this.authorService.findAll();
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Get('/id/:id')
   @UsePipes(ValidationPipe)
   async findUnique(@Param('id', ParseIntPipe) id: number): Promise<Authors> {
     return await this.authorService.findUnique(id);
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Get('/name/:name')
   @UsePipes(ValidationPipe)
   async findName(@Param('name') name: string): Promise<Authors[]> {
     return await this.authorService.findByName(name);
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Patch('/update/:id')
   //@UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
@@ -58,6 +63,7 @@ export class AuthorController {
     return await this.authorService.update(id, updateAuthorDto);
   }
 
+  @ApiResponse({ status: 404, description: 'Não encontrado.' })
   @Delete('delete/:id')
   //@UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
