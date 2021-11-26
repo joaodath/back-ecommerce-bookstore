3146c9ee4c0214543669d92ad599517b94f40381
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-11-26 01:26:22 -0300

    fix: adicionar IsOptional em propriedades opcionais

--- src/cart/dto/update-item.dto.ts
+++ src/cart/dto/update-item.dto.ts
@@ -1,12 +1,14 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class UpdateItemDto {
+  @IsOptional()
   @IsNumber()
   bookId?: number;
 
   @IsNumber()
   quantity: number;
 
+  @IsOptional()
   @IsNumber()
   shoppingCartId?: number;
 
